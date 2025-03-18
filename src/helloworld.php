ee749b03ea8ed92c64722e8261c485d4624c13e7
Do Trong Anh <64917124+DoTrongAnh@users.noreply.github.com>
2025-03-17 21:25:42 -0600

    Create helloworld.php

---
+++ src/helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php 
+          echo '<p>Hello World</p>';
+          echo "<p>You are using $_SERVER['HTTP_USER_AGENT']</p>";
+        ?>
+    </body>
+</html>
