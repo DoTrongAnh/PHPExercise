81bb02832d1a34eb7025cbbb508558a36dfbc219
Do Trong Anh <64917124+DoTrongAnh@users.noreply.github.com>
2025-03-21 13:57:30 -0600

    Create combined_form.php

---
+++ src/combined_form.php
@@ -0,0 +1,18 @@
+<?php
+if ($_POST) {
+    echo $_POST['personal']['name'];
+    echo $_POST['personal']['email'];
+    echo $_POST['beer'];
+}
+?>
+<form action="" method="post">
+    Name:  <input type="text" name="personal[name]" /><br />
+    Email: <input type="text" name="personal[email]" /><br />
+    Beer: <br />
+    <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbräu</option>
+    </select><br />
+    <input type="submit" value="submit me!" />
+</form>
